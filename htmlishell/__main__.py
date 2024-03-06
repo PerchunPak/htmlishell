186ba53a28472a3a70d349918936dcd9980cfac1
PerchunPak <git@perchun.it>
2024-03-06 17:31:08 +0100

    Modify code to use `Parser` class

--- htmlishell/__main__.py
+++ htmlishell/__main__.py
@@ -1,24 +1,12 @@
 import sys
 import pathlib
-from bs4 import BeautifulSoup
 import subprocess
+from htmlishell.parsing import Parser
 
 
 def main(file_to_read: str) -> None:
     file_path = pathlib.Path(file_to_read)
-    with file_path.open("r") as file:
-        soup = BeautifulSoup(file, "html.parser")
-
-    result = ""
-    for command in soup.children:
-        if command == "\n":
-            continue
-        result = result + "\n" + command.name
-        for argument in command.children:
-            if argument == "\n":
-                continue
-            result += " --" + argument.name
-    result = result.strip()
+    result = Parser.parse(file_path.read_text())
     subprocess.run(result, shell=True, check=True)
 
 
