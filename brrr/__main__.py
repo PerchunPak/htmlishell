e3eb37c651d96f184d5dfd80ad66a866a20db61a
PerchunPak <git@perchun.it>
2024-03-06 14:58:21 +0100

    Remove unnecessary code

--- brrr/__main__.py
+++ brrr/__main__.py
@@ -1,20 +1,6 @@
-import asyncio
-
-from loguru import logger
-
-from src import utils
-from src.config import Config
-
-
-async def main() -> None:
-    utils.setup_logging()
-    logger.info("Hello World!")
-
-    Config()
-    utils.start_sentry()
-    await utils.start_apykuma()
-    # start app here
+def main() -> None:
+    ...  # start app here
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    main()
