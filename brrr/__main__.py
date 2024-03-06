8e54c05dc2601822104f9bf92359616496d194a0
PerchunPak <git@perchun.it>
2024-03-06 16:24:49 +0100

    Add minimal realization

--- brrr/__main__.py
+++ brrr/__main__.py
@@ -1,6 +1,26 @@
-def main() -> None:
-    ...  # start app here
+import sys
+import pathlib
+from bs4 import BeautifulSoup
+import subprocess
+
+
+def main(file_to_read: str) -> None:
+    file_path = pathlib.Path(file_to_read)
+    with file_path.open("r") as file:
+        soup = BeautifulSoup(file, "html.parser")
+
+    result = ""
+    for command in soup.children:
+        if command == "\n":
+            continue
+        result = result + "\n" + command.name
+        for argument in command.children:
+            if argument == "\n":
+                continue
+            result += " --" + argument.name
+    result = result.strip()
+    subprocess.run(result, shell=True, check=True)
 
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv[1])
