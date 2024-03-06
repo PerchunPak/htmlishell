e712afa6ddd0d18347c3695a14773e1e3554d279
PerchunPak <git@perchun.it>
2024-03-06 17:07:41 +0100

    Better simple `ls -al`

---
+++ brrr/parsing.py
@@ -0,0 +1,32 @@
+from bs4 import BeautifulSoup
+from bs4.element import Tag
+
+
+class Parser:
+    def __init__(self, soup: BeautifulSoup) -> None:
+        self.soup = soup
+
+    @classmethod
+    def parse(cls, as_string: str) -> str:
+        soup = BeautifulSoup(as_string, "html.parser")
+        self = cls(soup)
+
+        result = ""
+        for command in soup.children:
+            if command == "\n":
+                continue
+            result += self._parse_command(command)
+            for argument in command.children:
+                if argument == "\n":
+                    continue
+                result += self._parse_argument(argument)
+
+        return result.strip()
+
+    def _parse_command(self, command: Tag) -> str:
+        return "\n" + command.name
+
+    def _parse_argument(self, argument: Tag) -> str:
+        if argument.get_attribute_list("short")[0] == "true":
+            return " -" + argument.name
+        return " --" + argument.name
---
+++ tests/test_parsing.py
@@ -0,0 +1,26 @@
+from brrr.parsing import Parser
+
+
+def test_ls_all():
+    assert Parser.parse("""
+        <ls>
+            <all />
+        </ls>
+    """) == "ls --all"
+
+
+def test_ls_long():
+    assert Parser.parse("""
+        <ls>
+            <l short=true />
+        </ls>
+    """) == "ls -l"
+
+
+def test_ls_all_long():
+    assert Parser.parse("""
+        <ls>
+            <all />
+            <l short=true />
+        </ls>
+    """) == "ls --all -l"
