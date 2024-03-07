6092e749a8b173a31e8dc43e97aad4f5da0a4962
PerchunPak <git@perchun.it>
2024-03-07 23:34:35 +0100

    Fix it for pipx

--- htmlishell/__main__.py
+++ htmlishell/__main__.py
@@ -1,15 +1,19 @@
 import pathlib
 import subprocess
 import sys
+import typing as t
 
 from htmlishell.parsing import Parser
 
 
-def main(file_to_read: str) -> None:
+def main(file_to_read: t.Optional[str] = None) -> None:
+    if file_to_read is None:
+        file_to_read = sys.argv[1]
+
     file_path = pathlib.Path(file_to_read)
     result = Parser.parse(file_path.read_text())
     subprocess.run(result, shell=True, check=True)
 
 
 if __name__ == "__main__":
-    main(sys.argv[1])
+    main()
